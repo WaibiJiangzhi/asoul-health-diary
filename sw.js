1fe2feac27fff1e305478caea707ce0d91a74e27
WaibiJiangzhi <2399250976@qq.com>
2026-08-20 00:36:15 +0800

    1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "asoul-life-v28-0827";
+const VERSION = "asoul-life-v29-0828";
 const SHELL_CACHE = `${VERSION}-shell`;
 const IMAGE_CACHE = `${VERSION}-images`;
 const SHELL = [
@@ -8,7 +8,7 @@ const SHELL = [
   "./weekly-planner.css?v=0821",
   "./weekly-polish.css?v=0821",
   "./complete-polish.css?v=0821",
-  "./mobile-app.css?v=0827",
+  "./mobile-app.css?v=0828",
   "./data-model.js?v=0821",
   "./stickers.js?v=0821",
   "./冷笑话.js?v=0821",
